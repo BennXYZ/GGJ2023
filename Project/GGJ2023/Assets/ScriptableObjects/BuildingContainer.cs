aa969eaecf4544e3b0727f6ec694d820dfbec632
MaxPlay <maxplayyt@gmail.com>
2023-02-05 11:59:34 +0100

    Separated the logic of the two game states into separate classes that are just switched  -> They receive an update call when active and a shutdown/startup on toggle Build mode already kinda works but does not care for existing locations, yet

--- Project/GGJ2023/Assets/ScriptableObjects/BuildingContainer.cs
+++ Project/GGJ2023/Assets/ScriptableObjects/BuildingContainer.cs
@@ -22,22 +22,22 @@ public class BuildingContainer : ScriptableObject
     [field: SerializeField]
     public Color PreviewValid { get; private set; }
 
-    public IReadOnlyList<Building> GetBuildings(WorldGameManager.PlayerType playerId)
+    public IReadOnlyList<Building> GetBuildings(PlayerType playerId)
     {
         switch (playerId)
         {
-            case WorldGameManager.PlayerType.Over: return buildingsOver;
-            case WorldGameManager.PlayerType.Under: return buildingsUnder;
+            case PlayerType.Over: return buildingsOver;
+            case PlayerType.Under: return buildingsUnder;
             default: return null;
         }
     }
 
-    public IReadOnlyList<Transform> GetWorldSegments (WorldGameManager.PlayerType playerId)
+    public IReadOnlyList<Transform> GetWorldSegments (PlayerType playerId)
     {
         switch (playerId)
         {
-            case WorldGameManager.PlayerType.Over: return worldSegmentsOver;
-            case WorldGameManager.PlayerType.Under: return worldSegmentsUnder;
+            case PlayerType.Over:  return worldSegmentsOver;
+            case PlayerType.Under: return worldSegmentsUnder;
             default: return null;
         }
     }
---
+++ Project/GGJ2023/Assets/Scripts/WorldPlayMode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+internal class WorldPlayMode : WorldMode
+{
+    public WorldPlayMode(WorldGameManager worldGameManager) : base(worldGameManager)
+    {
+    }
+
+    public void MovePositive()
+    {
+
+    }
+
+    public void MoveNegative()
+    {
+
+    }
+
+    public override void Shutdown()
+    {
+
+    }
+
+    public override void Startup()
+    {
+
+    }
+
+    public override void Update()
+    {
+    }
+}
